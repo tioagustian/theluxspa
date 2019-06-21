3acbdcb3c07589d6d49ce9d83ec969f2ed196561
TioA <tio.agustian94@gmail.com>
2019-06-21 16:51:37 +0700

    Add midtrans library

---
+++ application/modules/payment/controllers/Token.php
@@ -0,0 +1,123 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Token extends MX_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -  
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in 
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see http://codeigniter.com/user_guide/general/urls.html
+	 */
+
+
+	public function __construct()
+    {
+        parent::__construct();
+        $params = array('server_key' => 'SB-Mid-server-9LQFGZYlC0TCqIqtUWEXLJpb', 'production' => false);
+		$this->load->library('midtrans');
+		$this->load->model('order');
+		$this->midtrans->config($params);
+		$this->load->helper('url');	
+    }
+
+    public function get($invoice = '')
+    {
+
+    	if (!$invoice) {
+    		return;
+    	}
+
+    	$data = $this->order->getServiceOrder($invoice);
+		// Required
+		$transaction_details = array(
+		  'order_id' => $invoice,
+		  'gross_amount' => $data['orders']->total, // no decimal allowed for creditcard
+		);
+
+		// Optional
+		$item1_details = array(
+		  'id' => $data['orders']->code,
+		  'price' => $data['orders']->total,
+		  'quantity' => '1',
+		  'name' => $data['orders']->service.' x '.$data['orders']->duration . 'menit'
+		);
+
+		// Optional
+		$item_details = array ($item1_details);
+
+		// Optional
+		$billing_address = array(
+		  'first_name'    => "Andri",
+		  'last_name'     => "Litani",
+		  'address'       => "Mangga 20",
+		  'city'          => "Jakarta",
+		  'postal_code'   => "16602",
+		  'phone'         => "081122334455",
+		  'country_code'  => 'IDN'
+		);
+
+		// Optional
+		$shipping_address = array(
+		  'first_name'    => "Obet",
+		  'last_name'     => "Supriadi",
+		  'address'       => "Manggis 90",
+		  'city'          => "Jakarta",
+		  'postal_code'   => "16601",
+		  'phone'         => "08113366345",
+		  'country_code'  => 'IDN'
+		);
+
+		// Optional
+		$customer_details = array(
+		  'first_name'    => $data['invoice_detail']->name,
+		  // 'last_name'     => "Litani",
+		  'email'         => $data['invoice_detail']->email,
+		  'phone'         => $data['invoice_detail']->phone
+		  // 'billing_address'  => $billing_address,
+		  // 'shipping_address' => $shipping_address
+		);
+
+		// Data yang akan dikirim untuk request redirect_url.
+        $credit_card['secure'] = true;
+        //ser save_card true to enable oneclick or 2click
+        //$credit_card['save_card'] = true;
+
+        $time = time();
+        $custom_expiry = array(
+            'start_time' => date("Y-m-d H:i:s O",$time),
+            'unit' => 'minute', 
+            'duration'  => 2
+        );
+        
+        $transaction_data = array(
+            'transaction_details'=> $transaction_details,
+            'item_details'       => $item_details,
+            'customer_details'   => $customer_details,
+            'credit_card'        => $credit_card,
+            'expiry'             => $custom_expiry
+        );
+
+		error_log(json_encode($transaction_data));
+		$snapToken = $this->midtrans->getSnapToken($transaction_data);
+		error_log($snapToken);
+		echo $snapToken;
+    }
+
+    public function finish()
+    {
+    	$result = json_decode($this->input->post('result_data'));
+    	echo 'RESULT <br><pre>';
+    	var_dump($result);
+    	echo '</pre>' ;
+
+    }
+}
